6b9d0072019e95bd5cce3e910b9b325b5137567c
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2019-10-06 21:30:36 +0300

    добавлены методы создания игроков и выдачу им случайных букв

--- main/resources/player.py
+++ main/resources/player.py
@@ -1,8 +1,13 @@
+from resources.scrabble import Scrabble
+
+
 class Player:
-    """Класс игрока с полями score и user_name"""
-    def __init__(self, score, name):
+    """Класс игрока с полями score, user_name и letters"""
+
+    def __init__(self, score, name, letters):
         self.score = score
         self._user_name = name
+        self.letters = letters
 
     def get_name(self):
         return self._user_name
