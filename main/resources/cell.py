97581e46f87de17388f8c972f195f6f08965ed1e
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2019-10-06 20:24:40 +0300

    Добавлен словарь слов рефактор __init__, добавлено поле mod_type в класс Cell

--- main/resources/cell.py
+++ main/resources/cell.py
@@ -1,8 +1,9 @@
 class Cell:
     """Ячейка игрового поля"""
-    def __init__(self, letter='*', mod=1):
+    def __init__(self, type_=None, letter='*', mod=1):
         self.cur_letter = letter # желательно проверять, что ввелась именно строка длиной 1 и это буква
         self.modifier = mod
+        self.mod_type = type_
 
     def set_letter(self, letter):  # изменяет букву в ячейке и возвращает количество очков за эту букву
         pass
--- main/resources/scrabble.py
+++ main/resources/scrabble.py
@@ -4,45 +4,56 @@ from .cell import Cell
 
 class Scrabble:
     """Главный игровой класс"""
-    # !решить как загружать бд со словами!
-    def __init__(self): # заставляет поле ячейками с дефолтным значением (не знаю пока какое взять)
-        self.board = []      # и где-то здесь же выбор режима игры
+
+    def _fill_board(self):
+        """Заполняет игровое поле начальными значениями"""
+        self.board = list()
         for i in range(15):
-            self.board.append([])
+            self.board.append(list())
             for j in range(15):
                 if i == j or i == 14 - j:
                     if j == 0 or j == 14:
-                        self.board[i].append(Cell(letter='3w', mod='3w'))
+                        self.board[i].append(Cell("word", '3w', 3))
                     elif 0 < j < 5 or 9 < j < 14:
-                        self.board[i].append(Cell(letter='2w', mod='2w'))
+                        self.board[i].append(Cell("word", '2w', 2))
                     elif j == 5 or j == 9:
-                        self.board[i].append(Cell(letter='3l', mod='3l'))
+                        self.board[i].append(Cell("letter", '3l', 3))
                     elif j == 6 or j == 8:
-                        self.board[i].append(Cell(letter='2l', mod='2l'))
+                        self.board[i].append(Cell("letter", '2l', 2))
                     else:
                         self.board[i].append(Cell())
                 elif i == j - 4 or i == 18 - j:
                     if j == 5 or j == 13:
-                        self.board[i].append(Cell(letter='3l', mod='3l'))
+                        self.board[i].append(Cell("letter", '3l', 3))
                     elif 5 < j < 8 or 10 < j < 13:
-                        self.board[i].append(Cell(letter='2l', mod='2l'))
+                        self.board[i].append(Cell("letter", '2l', 2))
                     else:
                         self.board[i].append(Cell())
                 elif i == j + 4 or i == 10 - j:
                     if j == 1 or j == 9:
-                        self.board[i].append(Cell(letter='3l', mod='3l'))
+                        self.board[i].append(Cell("letter", '3l', 3))
                     elif 1 < j < 4 or j == 8:
-                        self.board[i].append(Cell(letter='2l', mod='2l'))
+                        self.board[i].append(Cell("letter", '2l', 2))
                     else:
                         self.board[i].append(Cell())
                 elif (i == 7 and (j == 0 or j == 14)) or (j == 7 and (i == 0 or i == 14)):
-                    self.board[i].append(Cell(letter='3w', mod='3w'))
+                    self.board[i].append(Cell("word", '3w', 3))
                 elif ((i == 0 or i == 14) and (j == 3 or j == 11)) or ((i == 3 or i == 11) and (j == 0 or j == 14)):
-                    self.board[i].append(Cell(letter='2l', mod='2l'))
+                    self.board[i].append(Cell("letter", '2l', 2))
                 else:
                     self.board[i].append(Cell())
 
-    def print_board(self): # выводит игровое поле, имя игрока, чей сейчас ход и его буквы
+    def _get_dict(self):
+        """Получение словаря возможных слов"""
+        f = open("resources/word_rus.txt", encoding="utf-8")
+        self._dict = set(f.read().splitlines())
+        f.close()
+
+    def __init__(self):
+        self._fill_board()
+        self._get_dict()
+
+    def print_board(self):  # TODO: имя и буквы текущего игрока после поля
         for i in range(15):
             for j in range(15):
                 print('{:>3}'.format(self.board[i][j].cur_letter), end='')
